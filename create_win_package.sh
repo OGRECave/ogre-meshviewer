f2b22e2aa07e0241baaf5e910891e3df445cda8f
Pavel Rojtberg <rojtberg@gmail.com>
2022-11-18 20:59:05 +0100

    update ogre to 13.5.2 release

--- create_win_package.sh
+++ create_win_package.sh
@@ -3,20 +3,23 @@
 # This should work on Windows (MinGW) and Linux
 # for MinGW use e.g. https://gitforwindows.org/
 
-curl -L https://dl.cloudsmith.io/public/ogrecave/ogre/raw/versions/v13.4.0/ogre-sdk-v13.4.0-msvc141-x64.zip -o ogre-sdk.zip
-curl -LO https://www.python.org/ftp/python/3.7.9/python-3.7.9-embed-amd64.zip
-unzip python-3.7.9-embed-amd64.zip -d package
+mkdir winpkg
+cd winpkg
+
+curl -L https://dl.cloudsmith.io/public/ogrecave/ogre/raw/versions/v13.5.2/ogre-sdk-v13.5.2-msvc141-x64.zip -o ogre-sdk.zip
+curl -LO https://www.python.org/ftp/python/3.10.8/python-3.10.8-embed-amd64.zip
+unzip python-3.10.8-embed-amd64.zip -d package
 unzip ogre-sdk.zip
 
 # main
-cp ogre*py ogre*bat LICENSE README.md package/
+cp ../ogre*py ../ogre*bat ../LICENSE ../README.md package/
 
 # copy ogre parts
-cp -R lib/python3.7/dist-packages/Ogre package
+cp -R lib/python3.10/dist-packages/Ogre package
 # components
 cp bin/OgreMain.dll bin/OgreBites.dll bin/OgreOverlay.dll bin/OgreRTShaderSystem.dll bin/OgreTerrain.dll bin/OgrePaging.dll package
 # plugins
-cp bin/Codec*dll bin/RenderSystem*dll bin/Plugin_DotScene.dll package
+cp bin/Codec*dll bin/RenderSystem*dll bin/Plugin_DotScene.dll bin/Plugin_GLSLangProgramManager.dll package
 # deps
 cp bin/SDL2.dll bin/zlib.dll package
 
@@ -25,10 +28,12 @@ head -10 bin/plugins.cfg > package/plugins.cfg
 echo Plugin=Codec_STBI >> package/plugins.cfg
 echo Plugin=Codec_Assimp >> package/plugins.cfg
 echo Plugin=Plugin_DotScene >> package/plugins.cfg
+echo Plugin=RenderSystem_Vulkan >> package/plugins.cfg
+echo Plugin=Plugin_GLSLangProgramManager >> package/plugins.cfg
 
 # resources
-cp win_resources.cfg package/resources.cfg
+cp ../win_resources.cfg package/resources.cfg
 cp -R Media/RTShaderLib Media/Main package/
 cp -R Media/packs/SdkTrays.zip package/
 
-mv package ogre-meshviewer_22.06-win64
+mv package ogre-meshviewer_22.11-win64

c0d435dff35de4c027c9cec28ae671c12c98d2ef
Guillermo Ojea Quintana <bill_notgates@yahoo.com>
2024-02-12 15:10:47 -0300

    Add Wireframe mode to view settings (#25)
    Co-authored-by: Guillermo Ojea Quintana <gojeaqui@redhat.com>

--- ogre_mesh_viewer.py
+++ ogre_mesh_viewer.py
@@ -223,6 +223,9 @@ class MeshViewerGui(Ogre.RenderTargetListener):
                     self.app._toggle_axes()
                 if ImGui.MenuItem("Show Bounding Box", "B", enode.getShowBoundingBox()):
                     self.app._toggle_bbox()
+                if ImGui.MenuItem("Wireframe Mode", "W", app.cam.getPolygonMode() == Ogre.PM_WIREFRAME):
+                    self.app._toggle_wireframe_mode()
+
                 if entity.hasSkeleton() and ImGui.MenuItem("Show Skeleton", None, entity.getDisplaySkeleton()):
                     entity.setDisplaySkeleton(not entity.getDisplaySkeleton())
                 ImGui.EndMenu()
@@ -405,6 +408,8 @@ class MeshViewer(OgreBites.ApplicationContext, OgreBites.InputListener):
             self._toggle_axes()
         elif evt.keysym.sym == ord("p"):
             self._save_screenshot()
+        elif evt.keysym.sym == ord("w"):
+            self._toggle_wireframe_mode()
 
         return True
 
@@ -427,6 +432,14 @@ class MeshViewer(OgreBites.ApplicationContext, OgreBites.InputListener):
 
         enode = self.entity.getParentSceneNode()
         enode.showBoundingBox(not enode.getShowBoundingBox())
+
+    def _toggle_wireframe_mode(self):
+        polygon_mode = self.cam.getPolygonMode()
+
+        if polygon_mode == Ogre.PM_SOLID:
+            self.cam.setPolygonMode(Ogre.PM_WIREFRAME)
+        if polygon_mode == Ogre.PM_WIREFRAME:
+            self.cam.setPolygonMode(Ogre.PM_SOLID)
 
     def _toggle_axes(self):
         if not self.axes_visible:
